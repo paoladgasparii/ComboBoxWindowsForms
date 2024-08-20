2346ee71e02f92a9c37cce7e0392838525858459
Usuario <Usuario@CPD03-M03>
2024-08-20 11:53:04 -0300

    Adicionar arquivos de projeto.

---
+++ ComboBoxWindowsForms/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace ComboBoxWindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.btnInserir = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cboLista = new System.Windows.Forms.ComboBox();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblQtde = new System.Windows.Forms.Label();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIndice = new System.Windows.Forms.TextBox();
+            this.txtConteudo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(113, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Texto:";
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(176, 64);
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(483, 20);
+            this.txtTexto.TabIndex = 1;
+            // 
+            // btnInserir
+            // 
+            this.btnInserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInserir.Location = new System.Drawing.Point(205, 121);
+            this.btnInserir.Name = "btnInserir";
+            this.btnInserir.Size = new System.Drawing.Size(158, 48);
+            this.btnInserir.TabIndex = 2;
+            this.btnInserir.Text = "Inserir no combo";
+            this.btnInserir.UseVisualStyleBackColor = true;
+            this.btnInserir.Click += new System.EventHandler(this.btnInserir_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOrdenar.Location = new System.Drawing.Point(416, 121);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(158, 48);
+            this.btnOrdenar.TabIndex = 3;
+            this.btnOrdenar.Text = "Ordenar o combo";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(114, 205);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lista:";
+            // 
+            // cboLista
+            // 
+            this.cboLista.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.cboLista.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboLista.FormattingEnabled = true;
+            this.cboLista.Location = new System.Drawing.Point(176, 205);
+            this.cboLista.Name = "cboLista";
+            this.cboLista.Size = new System.Drawing.Size(483, 21);
+            this.cboLista.TabIndex = 5;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemover.Location = new System.Drawing.Point(311, 262);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(158, 48);
+            this.btnRemover.TabIndex = 6;
+            this.btnRemover.Text = "Remover do combo";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(265, 344);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Quantidade de itens:";
+            // 
+            // lblQtde
+            // 
+            this.lblQtde.AutoSize = true;
+            this.lblQtde.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQtde.Location = new System.Drawing.Point(423, 345);
+            this.lblQtde.Name = "lblQtde";
+            this.lblQtde.Size = new System.Drawing.Size(12, 18);
+            this.lblQtde.TabIndex = 8;
+            this.lblQtde.Text = " ";
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExibir.Location = new System.Drawing.Point(311, 394);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(158, 48);
+            this.btnExibir.TabIndex = 9;
+            this.btnExibir.Text = "Exibir dados do combo";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(114, 487);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 18);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Índice do combo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(113, 542);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Texto do combo:";
+            // 
+            // txtIndice
+            // 
+            this.txtIndice.Location = new System.Drawing.Point(242, 487);
+            this.txtIndice.Name = "txtIndice";
+            this.txtIndice.Size = new System.Drawing.Size(417, 20);
+            this.txtIndice.TabIndex = 12;
+            // 
+            // txtConteudo
+            // 
+            this.txtConteudo.Location = new System.Drawing.Point(242, 539);
+            this.txtConteudo.Name = "txtConteudo";
+            this.txtConteudo.Size = new System.Drawing.Size(417, 20);
+            this.txtConteudo.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 622);
+            this.Controls.Add(this.txtConteudo);
+            this.Controls.Add(this.txtIndice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.lblQtde);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.cboLista);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnInserir);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.Button btnInserir;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboLista;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblQtde;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIndice;
+        private System.Windows.Forms.TextBox txtConteudo;
+    }
+}
+
---
+++ ComboBoxWindowsForms/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBoxWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInserir_Click(object sender, EventArgs e)
+        {
+            // Adiciona itens manualmente no ComboBox
+            cboLista.Items.Add(txtTexto.Text);
+
+            // Exibe a quantidade de iten do ComboBox
+            lblQtde.Text = cboLista.Items.Count.ToString();
+
+            // Limpa a caixa de texto
+            txtTexto.Clear();
+
+            // Move o foco para a caixa de texto
+            txtTexto.Select();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            // Ativa a propriedade para ordenar a lista do combo
+            cboLista.Sorted = true;
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            // Verifica se existe um item selecionado no combo
+            if (cboLista.SelectedIndex != -1)
+            {
+                // Remove o item selecionado do combo
+                cboLista.Items.RemoveAt(cboLista.SelectedIndex);
+
+                // Exibe a quantidade de itens do ComboBox
+                lblQtde.Text = cboLista.Items.Count.ToString();
+
+                // Limpa p texto do combo
+                cboLista.ResetText();
+            }
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e)
+        {
+            // Verifica se existe um item selecionado no combo
+            if (cboLista.SelectedIndex != -1)
+            {
+                // Exibe o conteúdo (texto do combo)
+                txtConteudo.Text = cboLista.SelectedItem.ToString();
+
+                // Exibe o índice (posição) do item no combo
+                txtIndice.Text = cboLista.SelectedIndex.ToString();
+            }
+        }
+    }
+}
